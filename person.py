1a61d1a20ed1803040e5e89bb6bf1e73cb662de1
Eri129-hub <waiphoo129@gmail.com>
2026-06-15 02:00:26 +0900

    Create person.py

---
+++ person.py
@@ -0,0 +1,13 @@
+class Person:
+
+    def __init__(self, name, age):
+
+        self.name = name
+
+        self.age = age
+
+person1 = Person("Eri", 20)
+
+print(person1.name)
+
+print(person1.age)
